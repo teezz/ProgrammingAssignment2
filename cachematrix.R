9c381c74318433136966ddabaf85f9b331cca019
teezz <info@reporting-online.com>
2015-09-24 12:46:14 +0200

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,20 @@
+# source("makeCacheMatrix.R")
+# e.g. x <- matrix(c(1, -2, 1, 1), nrow = 2)
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  set <- function(y) {
+        x <<- y
+  }
+
+  get <- function() x
+
+  list(set = set, get = get)
 
 }
 
